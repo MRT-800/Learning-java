84f45d924fed787f8a0c02d3df358819f05d4d92
MRT-800(Abhi) <abhishekthapaliya81@gmail.com>
2025-07-17 12:55:19 +0545

    reverse of a no. with palindrom check

--- src/Main.java
+++ src/Main.java
@@ -131,7 +131,7 @@ public class Main {
 
 
 //prime or not prime
-
+/*
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -156,5 +156,25 @@ public class Main {
     }
 }
 
+*/
 
-
+//reverse of a no. and palindrom
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp=n;
+        int rev =0;
+        while(n!=0){
+            rev=rev*10+n%10;
+            n=n/10;
+        }
+        System.out.println(rev);
+        if(temp==rev){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not Palindrome");
+        }
+    }
+}
