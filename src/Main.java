ee1af093337f3f2dfc291a6129afb30f01963d3f
MRT-800(Abhi) <abhishekthapaliya81@gmail.com>
2025-07-18 20:41:16 +0545

    Array taking inputs

--- src/Main.java
+++ src/Main.java
@@ -229,7 +229,7 @@ public class Main {
 //array
 
 
-
+/*
 public class Main {
     public static void main(String[] args) {
         // Create and initialize the array
@@ -241,6 +241,25 @@ public class Main {
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
+    }
+}
+
+*/
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i]= sc.nextInt();
+        }
+
+        System.out.println("You entered: " + Arrays.toString(numbers));
     }
 }
 
